2a229a7e5b8892725c2d6e4041cb0d2a0cb792be
treefroog <crs2017@gmail.com>
2017-01-25 19:01:34 -0500

    Removes all the uneeded junk

--- list-generator.ps1
+++ list-generator.ps1
@@ -1,1 +1,9 @@
 ﻿$programs = [System.Collections.Generic.List[System.Object]](choco list --localonly) #grabs the stuff installed
+$programs.RemoveAt(0) #removes the First line (junk)
+$programs.RemoveAt($programs.Count - 1) #Removes the last line (junk)
+
+$programs = $programs | ForEach-Object {
+    $_ = $_.Split(" ")[0]
+    $_
+    }
+Echo $programs
