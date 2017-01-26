19ce4e3ce83e2514bec75f7dc7fcc9b6a8994720
treefroog <crs2017@gmail.com>
2017-01-25 21:17:04 -0500

    Added the rest of the generator
    Auto-generates a powershell script

--- list-generator.ps1
+++ list-generator.ps1
@@ -5,8 +5,10 @@ $programs.RemoveAt(0) #removes the First line (junk)
 $programs.RemoveAt($programs.Count - 1) #Removes the last line (junk)
 
 $programs = $programs | ForEach-Object {
+
     $_ = $_.Split(" ")[0]
     $_
+
     }
 
 Echo $programs
@@ -15,16 +17,36 @@ $user_input = Read-Host -Prompt "`nDo you accept these programs? `nYes = [Y] No 
 $user_input = $user_input.ToLower()
 
 if ( $user_input -eq "y" ) { #if statements to read input
-    echo "later"
-    #do junk
+    
+    $output_file = $programs | ForEach-Object {
+        $_ = $_.Insert($_.Length, " -y")
+        $_ = $_.Insert(0, "cinst ")
+        $_
+        }
+
+    $output_file = [System.Collections.Generic.List[System.Object]]($output_file)
+    $output_file.Insert(0, "iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex")
+    $output_file.Insert(0, "`Clear-Host")
+    $output_file = $output_file + "Clear-Host"
+    $output_file = $output_file + "echo `"All done!`""
+    $output_file = $output_file + "Pause"
+    Clear-Host
+    $output_file >> choco_installer_script.ps1
+    echo "Done :)"
+    Pause
+
     }
 ElseIf ( $user_input -eq "n" ) {
+
     echo "`nBye bye `n"
     Pause
     Exit
+
     }
 Else {
+
     echo "`nNot valid input `n"
     Pause
     Exit
+
     }
